1b76fe40337a7a53713722cee91112dea33ef0b5
magicalworld4ever <93410518+magicalworld4ever@users.noreply.github.com>
2026-05-12 14:53:42 -0700

    .

--- src/incomplete code/QuestionPanel.java
+++ src/incomplete code/QuestionPanel.java
@@ -11,6 +11,11 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -22,24 +27,134 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+public class QuestionPanel
+{
+	public static void main(String[] args) 
+	{
+		JFrame frame = new JFrame("SHOWS HOW FILES COULD BE USED IN A GUI");
+		frame.setLayout(new BorderLayout());
+		CardForGameModuleFiles panel = new CardForGameModuleFiles();
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setSize(960, 600);
+		frame.setLocation(200, 140);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
 
-class QuestionPanel extends JPanel implements ActionListener
+class CardForGameModuleFiles extends JPanel
+{
+	private CardLayout listOfCards;
+	private GameData data;
+	
+	public CardForGameModuleFiles ( )
+	{
+		data = new GameData();
+		data.grabQuestionFromFile();
+		
+		setBackground(Color.BLACK);
+		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		listOfCards = new CardLayout();
+		setLayout(listOfCards);
+		
+		StartPanel first = new StartPanel(data, listOfCards, this);
+		add(first, "1");
+		
+		QuestionsPanel second = new QuestionsPanel(data, listOfCards, this);
+		add(second, "2");
+		
+		HighScoresPanel third = new HighScoresPanel(data, listOfCards, this);
+		add(third, "3");
+	}
+}
+
+class StartPanel extends JPanel implements ActionListener
 {
 	private GameData data;
-	private PageManager pm;
+	private CardLayout listOfCards;
+	private CardForGameModuleFiles primaryPanel;
+	private JTextField firstNameField, lastNameField;
+	
+	public StartPanel(GameData d, CardLayout c, CardForGameModuleFiles p)
+	{
+		data = d;
+		listOfCards = c;
+		primaryPanel = p;
+		
+		setBackground(Color.BLACK);
+		setLayout(new GridLayout(3, 1, 10, 10));
+		Font myFont = new Font("Tahoma", Font.BOLD, 22);
+		
+		JPanel firstNamePanel = new JPanel();
+		firstNamePanel.setBackground(Color.LIGHT_GRAY);
+		firstNamePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 70));
+		add(firstNamePanel);
+		
+		JLabel firstNamePrompt = new JLabel("First Name: ");
+		firstNamePrompt.setFont(myFont);
+		firstNamePanel.add(firstNamePrompt);
+		
+		firstNameField = new JTextField(16);
+		firstNameField.setMargin(new Insets(10,10,10,10));
+		firstNameField.setFont(myFont);
+		firstNamePanel.add(firstNameField);
+		
+		JPanel lastNamePanel = new JPanel();
+		lastNamePanel.setBackground(Color.LIGHT_GRAY);
+		lastNamePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 70));
+		add(lastNamePanel);
+		
+		JLabel lastNamePrompt = new JLabel("Last Name: ");
+		lastNamePrompt.setFont(myFont);
+		lastNamePanel.add(lastNamePrompt);
+		
+		lastNameField = new JTextField(16);
+		lastNameField.setMargin(new Insets(10,10,10,10));
+		lastNameField.setFont(myFont);
+		lastNamePanel.add(lastNameField);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(Color.LIGHT_GRAY);
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 70));
+		add(buttonPanel);
+		
+		JButton next = new JButton("NEXT PANEL");
+		next.setFont(myFont);
+		next.addActionListener(this);
+		buttonPanel.add(next);
+	}
+	
+	public void actionPerformed(ActionEvent evt) 
+	{
+		String command = evt.getActionCommand();
+		
+		if(command.equals("NEXT PANEL") && !firstNameField.getText().equals("") && !lastNameField.getText().equals(""))
+		{
+			data.setName(firstNameField.getText(), lastNameField.getText());
+			listOfCards.next(primaryPanel);
+		}
+	}
+}
+
+class QuestionsPanel extends JPanel implements ActionListener
+{
+	private GameData data;
+	private CardLayout listOfCards;
+	private CardForGameModuleFiles primaryPanel;
 	private ButtonGroup group;
 	private JTextArea questionArea;
 	private JRadioButton [] answer;
 	private JButton submit, nextQuestion, nextPanel;
 	
-	public QuestionPanel(PageManager pageMngr)
+	public QuestionsPanel(GameData d, CardLayout c, CardForGameModuleFiles p)
 	{
-		pm = pageMngr;
-		data = pm.getGameData();
+		data = d;
+		listOfCards = c;
+		primaryPanel = p;
 		
 		setBackground(Color.BLACK);
 		setLayout(new BorderLayout(10, 10));
-		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		Font myFont = new Font("Tahoma", Font.BOLD, 22);
 		
 		answer = new JRadioButton[4];
@@ -173,6 +288,98 @@ class QuestionPanel extends JPanel implements ActionListener
 	}
 }
 
+class HighScoresPanel extends JPanel implements ActionListener
+{
+	private GameData data;
+	private CardLayout listOfCards;
+	private CardForGameModuleFiles primaryPanel;
+	private JTextArea scoreInfo, highScoresArea;
+	
+	public HighScoresPanel(GameData d, CardLayout c, CardForGameModuleFiles p)
+	{
+		data = d;
+		listOfCards = c;
+		primaryPanel = p;
+		
+		Font myFont = new Font("Tahoma", Font.BOLD, 22);
+		
+		setLayout(new BorderLayout(20, 20));
+		setBackground(Color.GRAY);
+		setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		setFont(myFont);
+		
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(1, 2, 10, 10));
+		add(centerPanel, BorderLayout.CENTER);
+		
+		JPanel leftSidePanel = new JPanel();
+		leftSidePanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+		leftSidePanel.setLayout(new BorderLayout());
+		centerPanel.add(leftSidePanel, BorderLayout.CENTER);
+		
+		scoreInfo = new JTextArea("" + data.getCorrectCount(), 10, 20);
+		scoreInfo.setFont(myFont);
+		scoreInfo.setLineWrap(true);
+		scoreInfo.setWrapStyleWord(true);
+		scoreInfo.setOpaque(false);
+		scoreInfo.setEditable(false);
+		leftSidePanel.add(scoreInfo);
+		
+		JPanel rightSidePanel = new JPanel();
+		rightSidePanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+		rightSidePanel.setLayout(new BorderLayout());
+		centerPanel.add(rightSidePanel, BorderLayout.CENTER);
+		
+		highScoresArea = new JTextArea("" + data.getHighScores(), 10, 20);
+		highScoresArea.setFont(myFont);
+		highScoresArea.setLineWrap(true);
+		highScoresArea.setWrapStyleWord(true);
+		highScoresArea.setOpaque(false);
+		highScoresArea.setEditable(false);
+		highScoresArea.setMargin(new Insets(10,10,10,10));
+		JScrollPane scroller = new JScrollPane(highScoresArea);
+		rightSidePanel.add(scroller);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		JButton playAgain = new JButton("PLAY AGAIN");
+		playAgain.setFont(myFont);
+		playAgain.addActionListener(this);
+		buttonPanel.add(playAgain);
+		
+		JButton exit = new JButton("EXIT");
+		exit.setFont(myFont);
+		exit.addActionListener(this);
+		buttonPanel.add(exit);
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		scoreInfo.setText("" + data.toString());
+		highScoresArea.setText("" + data.getHighScores());
+		highScoresArea.setCaretPosition(0);
+	}
+	
+	public void actionPerformed(ActionEvent evt) 
+	{
+		String command = evt.getActionCommand();
+		
+		if(command.equals("PLAY AGAIN"))
+		{
+			data.saveToHighScores();
+			listOfCards.previous(primaryPanel);
+		}
+		else if(command.equals("EXIT"))
+		{
+			data.saveToHighScores();
+			System.exit(0);
+		}
+	}
+}
+
 class GameData
 {
 	private String first, last;
@@ -189,6 +396,10 @@ class GameData
 		last = "";
 		correctCount = 0;
 		resetAll();
+	}
+	
+	public GameData(PageManager pageManager) {
+		//TODO Auto-generated constructor stub
 	}
 
 	public void resetAll ( )
@@ -208,7 +419,7 @@ class GameData
 	public void grabQuestionFromFile ( )
 	{
 		Scanner inFile = null;
-		String fileName = "Questions.txt";
+		String fileName = "computerQuestions.txt";
 		File inputFile = new File(fileName);
 		try
 		{
@@ -220,10 +431,10 @@ class GameData
 			System.out.println(e);
 			System.exit(1);
 		}
-		int questionNumber = (int)(Math.random() * 65);
+		int questionNumber = (int)(Math.random() * 30);
 		while(chosenQuestions[questionNumber] == true)
 		{
-			questionNumber = (int)(Math.random() * 65);
+			questionNumber = (int)(Math.random() * 30);
 		}
 		chosenQuestions[questionNumber] = true;
 		questionCount++;
@@ -282,89 +493,89 @@ class GameData
 		correctCount++;
 	}
 	
-	/*public String toString ( )
-	{
-		if(lastGameCorrectCount > 2)
-		{
-			return "Congratulations, " + first + " " + last + ", you answered " + lastGameCorrectCount +
-				" out of 4 of the questions correctly.  Your name will be added to the list of high scores, shown to the right.  Good work!";
-		}
-		return "Good try " + first + " " + last + ", you answered " + lastGameCorrectCount +
-			" out of 4 of the questions correctly.  Keep working at it, and maybe next time your name will be added to the list of high scores!";
-	}
+	// public String toString ( )
+	// {
+	// 	if(lastGameCorrectCount > 2)
+	// 	{
+	// 		return "Congratulations, " + first + " " + last + ", you answered " + lastGameCorrectCount +
+	// 			" out of 4 of the questions correctly.  Your name will be added to the list of high scores, shown to the right.  Good work!";
+	// 	}
+	// 	return "Good try " + first + " " + last + ", you answered " + lastGameCorrectCount +
+	// 		" out of 4 of the questions correctly.  Keep working at it, and maybe next time your name will be added to the list of high scores!";
+	// }
 	
-	public String getHighScores ( )
-	{
-		String result = "";
-		String fileName = "highScores.txt";
-		Scanner inFile = null;
-		File inputFile = new File(fileName);
-		try 
-		{
-			inFile = new Scanner(inputFile);
-		} 
-		catch(FileNotFoundException e) 
-		{
-			System.err.printf("ERROR: Cannot open %s\n", fileName);
-			System.out.println(e);
-			System.exit(1);
-		}
-		while(inFile.hasNext()) 
-		{
-			String line = inFile.nextLine();
-			result += line + "\n";
-		}
-		return result;
-	}
+	// public String getHighScores ( )
+	// {
+	// 	String result = "";
+	// 	String fileName = "highScores.txt";
+	// 	Scanner inFile = null;
+	// 	File inputFile = new File(fileName);
+	// 	try 
+	// 	{
+	// 		inFile = new Scanner(inputFile);
+	// 	} 
+	// 	catch(FileNotFoundException e) 
+	// 	{
+	// 		System.err.printf("ERROR: Cannot open %s\n", fileName);
+	// 		System.out.println(e);
+	// 		System.exit(1);
+	// 	}
+	// 	while(inFile.hasNext()) 
+	// 	{
+	// 		String line = inFile.nextLine();
+	// 		result += line + "\n";
+	// 	}
+	// 	return result;
+	// }
 	
-	public void saveToHighScores ( )
-	{
-		if(lastGameCorrectCount >= 3)
-		{
-			String result = "";
-			boolean hasBeenAdded = false;
-			String fileName = "highScores.txt";
-			Scanner inFile = null;
-			File inputFile = new File(fileName);
-			try 
-			{
-				inFile = new Scanner(inputFile);
-			} 
-			catch(FileNotFoundException e) 
-			{
-				System.err.printf("ERROR: Cannot open %s\n", fileName);
-				System.out.println(e);
-				System.exit(1);
-			}
-			while(inFile.hasNext()) 
-			{
-				String line = inFile.nextLine();
-				if(!hasBeenAdded && Integer.parseInt("" + line.charAt(line.indexOf("/") - 1)) <= lastGameCorrectCount);
-				{
-					result += first + " " + last + " " + lastGameCorrectCount + "/4\n";
-					hasBeenAdded = true;
-				}
-				result += line + "\n";
-			}
-			if(!hasBeenAdded)
-			{
-				result += first + " " + last + " " + lastGameCorrectCount + "/4\n";
-			}
-			inFile.close();
+	// public void saveToHighScores ( )
+	// {
+	// 	if(lastGameCorrectCount >= 3)
+	// 	{
+	// 		String result = "";
+	// 		boolean hasBeenAdded = false;
+	// 		String fileName = "highScores.txt";
+	// 		Scanner inFile = null;
+	// 		File inputFile = new File(fileName);
+	// 		try 
+	// 		{
+	// 			inFile = new Scanner(inputFile);
+	// 		} 
+	// 		catch(FileNotFoundException e) 
+	// 		{
+	// 			System.err.printf("ERROR: Cannot open %s\n", fileName);
+	// 			System.out.println(e);
+	// 			System.exit(1);
+	// 		}
+	// 		while(inFile.hasNext()) 
+	// 		{
+	// 			String line = inFile.nextLine();
+	// 			if(!hasBeenAdded && Integer.parseInt("" + line.charAt(line.indexOf("/") - 1)) <= lastGameCorrectCount);
+	// 			{
+	// 				result += first + " " + last + " " + lastGameCorrectCount + "/4\n";
+	// 				hasBeenAdded = true;
+	// 			}
+	// 			result += line + "\n";
+	// 		}
+	// 		if(!hasBeenAdded)
+	// 		{
+	// 			result += first + " " + last + " " + lastGameCorrectCount + "/4\n";
+	// 		}
+	// 		inFile.close();
 
-			File ioFile = new File("highScores.txt");
-			PrintWriter outFile = null;
-			try
-			{
-				outFile = new PrintWriter(ioFile);
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-				System.exit(1);
-			}
-			outFile.print(result);
-			outFile.close();
-		}*/
-	}
+	// 		File ioFile = new File("highScores.txt");
+	// 		PrintWriter outFile = null;
+	// 		try
+	// 		{
+	// 			outFile = new PrintWriter(ioFile);
+	// 		}
+	// 		catch(IOException e)
+	// 		{
+	// 			e.printStackTrace();
+	// 			System.exit(1);
+	// 		}
+	// 		outFile.print(result);
+	// 		outFile.close();
+	// 	}
+	// }
 }
