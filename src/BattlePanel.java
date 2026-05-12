576ccee0dd66faad998292fb7f5f76374ad2ad42
magicalworld4ever <93410518+magicalworld4ever@users.noreply.github.com>
2026-05-12 15:50:33 -0700

    .

---
+++ src/BattlePanel.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class BattlePanel extends JPanel implements ActionListener
+{
+    private GameData data;
+	private PageManager pm;
+
+    public BattlePanel(PageManager pageMngr)
+	{
+		pm = pageMngr;
+		data = pm.getGameData();
+
+        setLayout(new GridLayout(1, 4, 10, 10))
+
+        
+        Image userCard = PageManager.Functions.getImage("");
+
+
+
+    }
+}
